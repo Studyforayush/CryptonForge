ac2bafaf83cf25ed376628b83d30e027434ac775
Studyforayush <studyfroayush@gmail.com>
2025-11-04 22:23:01 +0530

    Update CryptonForge.sol

--- contracts/CryptonForge.sol
+++ contracts/CryptonForge.sol
@@ -1,21 +1,82 @@
-? Function 1: Mint (Forge) a new asset
-    function forgeAsset(string calldata _name, uint256 _value) external {
-        require(bytes(_name).length > 0, "Name cannot be empty");
-        require(_value > 0, "Value must be positive");
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-        assets.push(Asset(msg.sender, _name, _value, block.timestamp));
-        emit AssetForged(msg.sender, _name, _value);
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title CryptonForge
+ * @dev NFT minting and forging platform with upgradeable tokens
+ */
+contract CryptonForge is ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;
+
+    // Mapping tokenId to forging level (>=1)
+    mapping(uint256 => uint256) public tokenLevels;
+
+    event NFTMinted(address indexed to, uint256 indexed tokenId, string tokenURI);
+    event NFTForged(address indexed owner, uint256[] burnedTokenIds, uint256 newTokenId, uint256 newLevel);
+
+    constructor() ERC721("CryptonForge", "CFT") {}
+
+    /**
+     * @dev Owner can mint NFTs with initial level 1
+     * @param to Receiver of NFT
+     * @param tokenURI Metadata URI
+     */
+    function mintNFT(address to, string memory tokenURI) external onlyOwner returns (uint256) {
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenLevels[newTokenId] = 1;
+
+        emit NFTMinted(to, newTokenId, tokenURI);
+        return newTokenId;
     }
 
-    ? Function 3: Transfer ownership
-    function transferOwnership(address _newOwner) external {
-        require(msg.sender == owner, "Only owner can transfer ownership");
-        require(_newOwner != address(0), "Invalid address");
+    /**
+     * @dev User can forge by burning multiple owned NFTs to mint a higher level NFT
+     * @param tokenIds Token IDs user owns to burn
+     * @param newTokenURI Metadata URI for new forged NFT
+     */
+    function forgeNFT(uint256[] calldata tokenIds, string calldata newTokenURI) external returns (uint256) {
+        require(tokenIds.length >= 2, "At least two NFTs required to forge");
 
-        emit OwnershipTransferred(owner, _newOwner);
-        owner = _newOwner;
+        uint256 totalLevel = 0;
+
+        // Validate ownership and sum levels
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(ownerOf(tokenIds[i]) == msg.sender, "Must own all tokens");
+            totalLevel += tokenLevels[tokenIds[i]];
+        }
+
+        // Burn old tokens
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _burn(tokenIds[i]);
+            delete tokenLevels[tokenIds[i]];
+        }
+
+        // Mint new token with increased level
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, newTokenURI);
+
+        // Define new level as sum of levels (can be customized)
+        tokenLevels[newTokenId] = totalLevel;
+
+        emit NFTForged(msg.sender, tokenIds, newTokenId, totalLevel);
+
+        return newTokenId;
+    }
+
+    /**
+     * @dev Returns the forging level of a token
+     * @param tokenId Token ID
+     */
+    function getTokenLevel(uint256 tokenId) external view returns (uint256) {
+        require(_exists(tokenId), "Nonexistent token");
+        return tokenLevels[tokenId];
     }
 }
-// 
-update
-// 
